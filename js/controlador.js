a8a90664e8e3cd428c7ec1c75961ecf3932a723a
jdfiallos <juandanielfiallos@gmail.com>
2021-06-22 14:29:42 -0600

    [ADD] Modal to create app. Fixed CRUD of LS.

--- js/controlador.js
+++ js/controlador.js
@@ -17,7 +17,7 @@ var categorias = [];
   //Obtenemos la referencia a la tech de LocalStorage
   let localstorage = window.localStorage;
   //guardar, para hacerlo mas eficiente si ya existe que no lo vuelva a crear.
-  if (localstorage.getItem(categorias) == null) {
+  if (localstorage.getItem('categorias') == null) {
     let contador = 1;
     for (let i=0;i<5;i++){//Generar 5 categorias
         let categoria = {
@@ -48,65 +48,231 @@ var categorias = [];
         }
         categorias.push(categoria);
     }
+    /* Siempre se usara una variable para poder acceder. Tal como apps. */
     localstorage.setItem('categorias', JSON.stringify(categorias));
+    apps = JSON.parse(localstorage.getItem('categorias'));
   } else {
-    aplicaciones = localstorage.getItem(categorias);
+    apps = JSON.parse(localstorage.getItem('categorias'));
   };
-  
-  console.log(categorias);
 
-  /* Llenamos el select. */
-  for (let index = 0; index < categorias.length; index++) {
+  /* Llenamos el select con las categorias. */
+  for (let index = 0; index < apps.length; index++) {
     document.getElementById("selector").innerHTML +=
-    `<option value="${index}">${categorias[index].nombreCategoria}</option>`
+    `<option value="${index}">${apps[index].nombreCategoria}</option>`
     };
-
-
 })();
 
 
 function cargarApps() {
-    document.getElementById("app").innerHTML = null;
+    document.getElementById("app").innerHTML = '';
     seleccion = document.getElementById("selector").value;
-    for (let index = 0; index < categorias[seleccion].aplicaciones.length; index++) {
-        console.log(categorias[seleccion].aplicaciones[index]);
 
+    let localstorage = window.localStorage;
+    apps = JSON.parse(localstorage.getItem('categorias'));
+
+    for (let index = 0; index < apps[seleccion].aplicaciones.length; index++) {
         /* Generamos dinamicamente las estrellas */
-        let estrellas = categorias[seleccion].aplicaciones[index].calificacion;
+        let estrellas = apps[seleccion].aplicaciones[index].calificacion;
         let calificacionEstrella = '';
-    /* Aqui generamos la cantidad de estrellas */    
-    for (let index = 0; index < estrellas; index++) {
-        calificacionEstrella += '<i class="fas fa-star"></i>';
-    };
 
-    for (let index = 0; index < 5-estrellas; index++) {
-        calificacionEstrella += '<i class="far fa-star"></i>';
-    };
+        /* Aqui generamos la cantidad de estrellas */    
+        for (let index = 0; index < estrellas; index++) {
+            calificacionEstrella += '<i class="fas fa-star"></i>';
+        };
+        for (let index = 0; index < 5-estrellas; index++) {
+            calificacionEstrella += '<i class="far fa-star"></i>';
+        };
 
-
+        /* Tuvimos que hacer un replace ya que daba error al usar comillas. */
+        nombreApp = JSON.stringify(apps[seleccion].aplicaciones[index]).replace(/\"/g,"&quot;")
+            
+        /* Agregamos las apps dinamicamente. */
         document.getElementById("app").innerHTML += 
-    `
-    <!-- Esto lo manejaremos en js. -->
-    <div class="col-2 mb-2" >
-        <div class="card">
-            <div class=" imagen">
-                <img src="${categorias[seleccion].aplicaciones[index].icono}" class="card-img-top" alt="...">
+        `
+        <!-- Esto lo manejaremos en js. -->
+        <div class="col-2 mb-2">
+            <div class="card" onclick="generarModal(${nombreApp})" data-toggle="modal" data-target="#exampleModal">
+                <div class=" imagen">
+                    <img src="${apps[seleccion].aplicaciones[index].icono}" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body" style="margin-left: -10px; height: 115px;">
+                <h5 class="card-title" style="margin-top: -25px;">${apps[seleccion].aplicaciones[index].nombre}</h5>
+                <p class="card-text" style="margin-top: -10px;">${apps[seleccion].aplicaciones[index].desarrollador}</p>
+                <div class="mb-1" style="margin-top: -10px;">
+                    ${calificacionEstrella}
+                </div>
+                <h5>$3.15</h5>
+                </div>
             </div>
-            <div class="card-body" style="margin-left: -10px; height: 115px;">
-              <h5 class="card-title" style="margin-top: -25px;">${categorias[seleccion].aplicaciones[index].nombre}</h5>
-              <p class="card-text" style="margin-top: -10px;">${categorias[seleccion].aplicaciones[index].desarrollador}</p>
-              <div class="mb-1" style="margin-top: -10px;">
-                ${calificacionEstrella}
-              </div>
-              <h5>$3.15</h5>
+        </div>`
+    };
+}
+
+
+/* Esta es la modal de las apps. Con sus datos mas detallados. */
+function generarModal(app){
+    let estrella = '';
+
+    for (let index = 0;  index< app.calificacion; index++) {
+        estrella += '<i class="comentario fas fa-star"></i>';
+    }
+    for (let index = 0;  index< 5-app.calificacion; index++) {
+        estrella += '<i class="comentario far fa-star"></i>';
+    }
+
+    comment = '';
+    /* Hicimos una condicion ya que comentarios son por parte de usuarios y no el creador. */
+    if (app.comentarios == null) {
+        comment = '';
+    } else {
+        for (let index = 0; index < app.comentarios.length; index++) {
+            comment += `
+            <div class="row">
+                <div class="col-11 mx-auto">
+                    <hr>
+                </div>
+                <div class="col-3">
+                    <img class="col-11 mx-auto rounded-circle" src="./img/user.webp">
+                </div>
+                <div class="col-8">
+                    <div class="col-12" style="margin-left: -35px;">
+                        <h6>${app.comentarios[index].usuario}</h6>
+                    </div>
+                    <div class="col-12" style="margin-left: -35px; margin-top: -5px;">
+                        <p>
+                            ${app.comentarios[index].comentario}
+                        </p>
+                    </div>
+                </div>
+            </div>`;}
+    }
+    
+    disponible = '';
+    if (app.instalada == true) {
+        disponible = ''
+    } else {
+        disponible = `
+        <button type="button" class="btn btn-primary" style="background-color: green;" >
+        Instalar
+        </button>`
+    }
+
+    /* Llenamos los datos internos de la modal respectivamente de la app seleccionada. */
+    document.getElementById('exampleModal').innerHTML = ''
+    document.getElementById('exampleModal').innerHTML = 
+    `<div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-11 mx-auto">
+                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="${app.imagenes[0]}" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="${app.imagenes[1]}" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="${app.imagenes[2]}" class="d-block w-100" alt="...">
+                            </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-11 mx-auto">
+                        <hr>
+                    </div>
+                </div>
+                <div class="row">
+                    <img class="col-4 mb-5 mt-1 mx-auto" src="${app.icono}">
+                    <div class="col-7" >
+                        <div class="col-12" style="margin-left: -40px;">
+                            <h3>${app.nombre}</h3>
+                        </div>
+                        <div class="col-12" style="margin-left: -40px; margin-top: -5px;">
+                            <p>${app.desarrollador}</p>
+                        </div>
+                        <div class="col-12" style="margin-left: -40px; margin-top: -15px;">
+                            <label>
+                            ${app.descripcion}
+                            </label>
+                        </div>
+                        <div class="col-12" style="margin-left: -40px;">
+                            <h5>$3.11</h5>
+                        </div>
+                    </div>
+                    <div class="col-11 mx-auto">
+                        <hr>
+                    </div>
+                </div>
+                <div class="row">   
+                    <div class="col-11" style="text-align: center;">
+                        ${estrella}
+                        <i class="comentario">${app.calificacion}.0</i>
+                    </div>
+                </div>   
+                ${comment}
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                ${disponible}
             </div>
         </div>
-    </div>
-    `
-
-    };
-
-    
+    </div>`;
+    $('#exampleModal').modal('show');
 }
  
 
+function ModalAgregar() {
+    let localstorage = window.localStorage;
+    document.getElementById('Imagen').innerHTML = ''
+    seleccion = document.getElementById("selector").value;
+    appAgregar = JSON.parse(localstorage.getItem('categorias'));
+    sele = ''
+
+    for (let index = 0; index < appAgregar[seleccion].aplicaciones.length; index++) {
+        sele += `<option value="${appAgregar[seleccion].aplicaciones[index].icono}"> Imagen ${JSON.stringify(appAgregar[seleccion].aplicaciones[index].codigo)} </option>`;
+    }
+
+   document.getElementById('Imagen').innerHTML += `${sele}`; 
+   $('#exampleModal1').modal('show');
+}
+
+
+
+function AgregarApp() {
+    const app = {
+        codigo: document.getElementById('Codigo').value,
+        nombre: document.getElementById('Nombre').value,
+        descripcion: document.getElementById('Descripcion').value,
+        icono: document.getElementById('Imagen').value,
+        calificacion: 5,
+        desarrollador: document.getElementById('Desarrollador').value,
+        imagenes: {
+            0: "img/app-screenshots/1.webp",
+            1: "img/app-screenshots/2.webp",
+            2: "img/app-screenshots/3.webp",
+        },
+    };
+    
+    seleccion = document.getElementById("selector").value;
+    
+    let localstorage = window.localStorage;
+    cat = JSON.parse(localstorage.getItem('categorias'));
+
+    cat[seleccion].aplicaciones.push(app);
+
+    localstorage.setItem('categorias', JSON.stringify(cat));
+
+    cargarApps();
+    console.log('Agregada con exito.');
+    $('#exampleModal1').modal('hide');
+}
